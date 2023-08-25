72bd0851153cc724a2818fd85780b98cbdb7b4c9
colette <colettebajeczka@gmail.com>
2023-08-25 20:44:47 +0200

    add gpuAssert macro

--- basic.cu
+++ basic.cu
@@ -1,5 +1,17 @@
 #include<iostream>
 
+constexpr int GPU_BLOCK_SIZE = 256;
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 __host__ __device__ 
 __int128_t mypow(__int128_t x, __int128_t exponent) {
     __int128_t result = x;
