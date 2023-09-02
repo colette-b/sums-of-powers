67e34cbc3bfc995018860536fb796c14a2fc9001
colette <colettebajeczka@gmail.com>
2023-09-02 18:59:33 +0200

    update main_422

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
-main_n44:
-	nvcc --compiler-options -Wall main_n44.cu -o main_n44
+CC=nvcc
+CFLAGS=--compiler-options -Wall
+
+all: main_n44 main_422
+
+aux: basic.cu logging.cc sortedsums.cu
+
+main_n44: aux main_n44.cu 
+	$(CC) $(CFLAGS) main_n44.cu -o main_n44
+
+main_422: aux main_422.cu 
+	$(CC) $(CFLAGS) main_422.cu -o main_422
--- main_422.cu
+++ main_422.cu
@@ -11,13 +11,13 @@ struct LeqCondition {
 };
 
 int main() {
-    thrust::host_vector<data_t> powers;
-    thrust::host_vector<bool> dummy;
+    thrust::device_vector<data_t> powers;
+    thrust::host_vector<data_t> h_powers;
     int N = 1000000;
     for(data_t i = 0; i < N; i++) {
-        powers.push_back(i * i * i * i);
-        dummy.push_back(true);
+        h_powers.push_back(i * i * i * i);
     }
+    powers = h_powers;
     SortedSums<data_t, data_t, data_t, LeqCondition> ss(
         30 << 20,
         1 << 15,
